24229d90f5bd90bd379859f978ea26fe3fdad2e9
jeancarlos1808 <solanosagaljeancarlos@gmail.com>
2024-11-29 12:47:52 +0100

    Cambios realizados

--- app/src/main/java/com/examenjeancarlos1808/examenpmdm/fragments/Fragment2.java
+++ app/src/main/java/com/examenjeancarlos1808/examenpmdm/fragments/Fragment2.java
@@ -1,20 +1,26 @@
 package com.examenjeancarlos1808.examenpmdm.fragments;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.filament.View;
+import com.examenjeancarlos1808.examenpmdm.R;
 
 public class Fragment2 extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_2, container, false);
-        TextView usernameText = view.findViewById(R.id.username_text);
+        android.view.View view = inflater.inflate(R.layout.fragment_2, container, false);
+        TextView usernameText = ((android.view.View) view).findViewById(R.id.username_text);
 
         SharedPreferences prefs = requireContext().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
         String username = prefs.getString("username", "Desconocido");
